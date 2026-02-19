cc36dd7cd8ee28333cabedef5a3c5f0f9cf08fda
sandralin33 <sandral9@g.ucla.edu>
2026-02-19 15:39:43 -0800

    score text

--- Assets/Art/Mint.cs
+++ Assets/Art/Mint.cs
@@ -3,29 +3,24 @@ using UnityEngine;
 public class Mint : MonoBehaviour
 {
     [SerializeField] float speed = 20f;
-    private Rigidbody2D rb;
+    [SerializeField] float life = 3f;
 
-    [SerializeField] float life = 3f; // destroys after 3 seconds
+    private Rigidbody2D rb;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        rb.linearVelocity = Vector2.right * speed; // always moves right
+        rb.linearVelocity = Vector2.right * speed;
 
         Destroy(gameObject, life);
-    }
-
-    void Update()
-    {
-
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Enemy"))
         {
-            Destroy(collision.gameObject); // destroys enemy
-            Destroy(gameObject); // destroys bullet
+            Destroy(collision.gameObject);
+            Destroy(gameObject);
         }
     }
 }
